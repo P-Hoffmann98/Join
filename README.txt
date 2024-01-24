ff60484c6180e9fb4f359b09dca454372e1732d4
P-Hoffmann98 <patrick.hoffmann1998@yahoo.de>
2024-01-24 20:33:45 +0100

    Storage Funktion und weitere Odner sowie Dateien angelegt

---
+++ README.txt
@@ -0,0 +1,18 @@
+Function name: in camelCase / example -  boardFunctionName() or summaryFunctionName() etc...
+
+const name: in CapitalCase / example - const STORAGE_TOKEN etc...
+
+Global variable: in camelCase / example -  boardVariableName or summaryVariableName etc...
+
+ID name: in snake_case / example - board_id_name or summary_idname etc...
+
+Class name: in kebab-case / example - board-class-name or summary-class-name etc...
+
+__
+
+
+example to load other html-files into a div: '<div w3-include-html="header.html"></div>'
+
+__
+
+images in svg  or png if this version is smaller
---
+++ js/storage.js
@@ -0,0 +1,22 @@
+const STORAGE_TOKEN = "ZE2UHDN0ZNZ1NK5SZMO83CGMAD73WG0SLNIZYU6W";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
+}
+
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data.value;
+      }
+      throw `Could not find data with key "${key}".`;
+    });
+}
