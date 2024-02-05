7946ece58eaa936a8fcb090b2b1d21469660d481
MarcusLoo23 <info@loosen-elektrotechnik.de>
2024-02-05 14:46:36 +0100

    deleted renderfuncitons

--- js/add_task.js
+++ js/add_task.js
@@ -227,20 +227,6 @@ function markSelectedContacts(id) {
   }
 }
 
-function addTaskRenderSearchName(color) {
-  document.getElementById(
-    "add_task_select_user_box"
-  ).innerHTML += `                     
-          <div class="selectField" id="${resultId}" onclick="addStyleToSelectedContact(${resultId})">
-            <span class="selectInitial dFlexAiCenterJcCenter" style="background-color:${resultColor}">${resultInitials}</span>
-              <span class="selectName">${resultNames}</span>
-                <img src="./img/add_task_rectangle.svg" id="selectContactBox${resultId}">
-          </div>`;
-  if (color) {
-    document.getElementById(resultId).classList.add("selectedContact");
-  }
-}
-
 function addTaskShowMsg(param) {
   labelId = "add_task_label_" + param;
   mistakeId = "add_task_mistake_" + param;
@@ -272,19 +258,6 @@ function deleteContactFromTask(contactId) {
   let resultIdToDelete = assignedToAddTask.indexOf(contactId);
   assignedToAddTask.splice(resultIdToDelete, 1);
   renderSelectedContactsFromTask();
-}
-
-function renderSelectedContactsFromTask() {
-  document.getElementById("outputSelectedContacts").innerHTML = ``;
-  for (let i = 0; i < assignedToAddTask.length; i++) {
-    for (let j = 0; j < contacts.length; j++) {
-      if (assignedToAddTask[i] == contacts[j]["id"]) {
-        document.getElementById(
-          "outputSelectedContacts"
-        ).innerHTML += `<span class="initialsOverview" style="background-color:${contacts[j]["color"]}">${contacts[j]["initials"]}</span>`;
-      }
-    }
-  }
 }
 
 function selectCategory(param) {
@@ -414,20 +387,4 @@ function changeSubTaskImg() {
 function deleteSubtask(index) {
   subtaskAddTask.splice(index, 1);
   renderSubTasks();
-}
-
-function renderSubTasks() {
-  let output = document.getElementById("outputSubtasks");
-  output.innerHTML = ``;
-  for (let i = 0; i < subtaskAddTask.length; i++) {
-    output.innerHTML += `                        
-    <div ondblclick="editSubTask(${i})" class="cursorPointer li"  contenteditable="false">
-      &#8226;<span id="subtaskContent${i}"> ${subtaskAddTask[i]}</span>
-        <div class="container_img_subtask">
-          <img id="img_add_subtask_check${i}" src="./img/add_task_check_small.png" onclick="saveEditing(${i})" class="m-right20 cursorPointer d-none">
-          <img id="img_add_subtask${i}" src="./img/add_task_edit_small.png" onclick="editSubTask(${i})" class="m-right20 cursorPointer">
-          <img src="./img/add_task_delete_small.png" class="cursorPointer" onclick="deleteSubtask(${i})">
-        </div>
-    </div>`;
-  }
 }
