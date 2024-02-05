263199ea61408c108ac7d6189e2ad972c72e4985
P-Hoffmann98 <patrick.hoffmann1998@yahoo.de>
2024-02-05 14:34:50 +0100

    changes made for contact.html .css .js

--- js/storage.js
+++ js/storage.js
@@ -16,6 +16,23 @@ let contacts = [
   },
 ];
 let currentUser = [];
+let allColors = [
+  "#FF7A00",
+  "#FF5EB3",
+  "#6E52FF",
+  "#9327FF",
+  "#00BEE8",
+  "#1FD7C1",
+  "#FF745E",
+  "#FFA35E",
+  "#FC71FF",
+  "#FFC701",
+  "#0038FF",
+  "#C3FF2B",
+  "#FFE62B",
+  "#FF4646",
+  "#FFBB2B",
+];
 
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
