06b089fd59a1ef34bcfeccfbdb0dd97798047315
MarcusLoo23 <info@loosen-elektrotechnik.de>
2024-02-12 20:42:57 +0100

    functions to clear arrays tasks, users and contacts for testing

--- js/add_task.js
+++ js/add_task.js
@@ -7,8 +7,12 @@ async function addTaskInit() {
   renderSubTasks();
   loadCurrentUser();
   loadInitials();
-  //   let tasks = [];
-  //   await setItem("tasks", JSON.stringify(tasks));
+  // let tasks = [];
+  // await setItem("tasks", JSON.stringify(tasks));
+  // let contacts = [];
+  // await setItem("contacts", JSON.stringify(contacts));
+  // let users = [];
+  // await setItem("users", JSON.stringify(users));
 }
 
 let addTaskPrio = "medium";
