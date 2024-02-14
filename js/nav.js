acb8148dfe4228669687ba104454035d9819ac96
MarcusLoo23 <info@loosen-elektrotechnik.de>
2024-02-14 13:49:35 +0100

    edited classnames

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 function changeMenuLinkBgColor() {
   let currentPage = window.location.pathname;
-  let links = document.querySelectorAll(".navigation a");
+  let links = document.querySelectorAll(".nav-menu-point a");
+  console.log(currentPage);
+  console.log(links);
   links.forEach((link) => {
     if (link.href.includes(`${currentPage}`)) {
-      link.classList.add("activeMenuLink");
+      link.classList.add("active-menu-link");
     }
   });
 }
