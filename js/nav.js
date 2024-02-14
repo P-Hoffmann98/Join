d5f1d34df5b48180f6797490a4b01131b522770d
MarcusLoo23 <info@loosen-elektrotechnik.de>
2024-02-14 16:21:53 +0100

    console.log deleted and new class for querySelector included

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,6 @@
 function changeMenuLinkBgColor() {
   let currentPage = window.location.pathname;
-  let links = document.querySelectorAll(".nav-menu-point a");
-  console.log(currentPage);
-  console.log(links);
+  let links = document.querySelectorAll(".nav-menu a");
   links.forEach((link) => {
     if (link.href.includes(`${currentPage}`)) {
       link.classList.add("active-menu-link");
