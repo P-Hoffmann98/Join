cb196bb223e1b57feca0a94f10e3902fd9e626b9
MarcusLoo23 <info@loosen-elektrotechnik.de>
2024-02-14 18:01:17 +0100

    function openContextMenuHeader added

--- js/header.js
+++ js/header.js
@@ -2,3 +2,7 @@ function loadInitials() {
   document.getElementById("header_initials").innerHTML =
     currentUser["initials"];
 }
+
+function openContextMenuHeader() {
+  document.getElementById("context_menu_header").classList.toggle("d-none");
+}
