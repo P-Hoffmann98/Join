6c736129fe304302c67e8a2df3ee459660ad9955
Tobias Nölle <tobias.noelle@icloud.com>
2024-02-15 19:25:38 +0100

    board_add_task js: created board_add_task.js and added function boardShowAddTask

---
+++ js/board_add_task.js
@@ -0,0 +1,4 @@
+function boardShowAddTask() {
+    document.getElementById('board_task_add_task_main').classList.remove('d-none');
+    document.getElementById('board_task_add_task_main').classList.add('d-flex');
+}
