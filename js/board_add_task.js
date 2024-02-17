49ca57a6de1ab03b48042e62cdbf61fd6c79f1d3
Tobias Nölle <tobias.noelle@icloud.com>
2024-02-17 13:34:31 +0100

    board files: renmae templates*add_task.css to board_add_task.css and add_task.html to board_add_task_html and load files in board.html

--- js/board_add_task.js
+++ js/board_add_task.js
@@ -1,4 +1,5 @@
 function boardShowAddTask() {
+    addTaskInit();
     document.getElementById('board_task_add_task_main').classList.remove('d-none');
     document.getElementById('board_task_add_task_main').classList.add('d-flex');
 }
