e301dadf344f096ed780076da60e423815e9b72b
Tobias Nölle <tobias.noelle@icloud.com>
2024-02-24 13:31:37 +0100

    board_edit_task.js: added function editTaskLoadValues and function editTaskFillInput to show values into edit window

--- js/board_edit_task.js
+++ js/board_edit_task.js
@@ -1,4 +1,8 @@
+let taskRecordSet;
+
 function boardShowEditTask(tasksIndex) {
+    editTaskLoadValues(tasksIndex);
+    editTaskFillInput();
     document.getElementById('board_task_edit_task_main').classList.remove('d-none');
     document.getElementById('board_task_edit_task_main').classList.add('d-flex');
 }
@@ -7,4 +11,18 @@ function boardShowEditTask(tasksIndex) {
 function boardCloseEditTask() {
     document.getElementById('board_task_edit_task_main').classList.add('d-none');
     document.getElementById('board_task_edit_task_main').classList.remove('d-flex');
+}
+
+
+function editTaskLoadValues(tasksIndex) {
+    taskRecordSet = tasks[tasksIndex];
+    console.log(taskRecordSet);
+}
+
+
+function editTaskFillInput() {
+    document.getElementById('edit_task_title').value = taskRecordSet['title'];
+    document.getElementById('edit_task_description').value = taskRecordSet['description'];
+    /* document.getElementById('edit_task_due_date').defaultValue = taskRecordSet['dueDate']; */
+    document.getElementById('edit_task_due_date').value = taskRecordSet['dueDate'];
 }
