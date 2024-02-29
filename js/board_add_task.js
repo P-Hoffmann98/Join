f41f8c9de5da1fc09eb79d70b2ccded810c4320d
Tobias Nölle <tobias.noelle@icloud.com>
2024-02-29 19:01:28 +0100

    board_add_task.js: comment and cleaned

--- js/board_add_task.js
+++ js/board_add_task.js
@@ -1,3 +1,7 @@
+/**
+ * function display add task contex in board site
+ * @param {string} taskStatus status category
+ */
 function boardShowAddTask(taskStatus) {
     addTaskInit();
     document.getElementById('board_task_add_task_main').classList.remove('d-none');
@@ -6,6 +10,9 @@ function boardShowAddTask(taskStatus) {
 }
 
 
+/**
+ * function hide add task content by closing task or save task
+ */
 function boardCloseAddTask() {
     let tasksCategoryDiv = document.getElementById('board_add_task_div');
     tasksCategoryDiv.parentElement.classList.add('d-none');
